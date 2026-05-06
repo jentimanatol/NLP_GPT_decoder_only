a5662192da7adbe31f357a2af4fd9ef374c83a0f
Anatolie Jentimir <157539682+jentimanatol@users.noreply.github.com>
2026-05-06 12:15:00 -0400

    Update download_dataset.bat

--- download_dataset.bat
+++ download_dataset.bat
@@ -1,11 +1,11 @@
-
 @echo off
-
 cd /d %~dp0
 
 if not exist data mkdir data
 
+echo Downloading Tiny Shakespeare dataset...
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/karpathy/char-rnn/master/data/tinyshakespeare/input.txt -OutFile data\tinyshakespeare.txt"
 
-echo Dataset downloaded.
+echo Done.
+echo Saved to data\tinyshakespeare.txt
 pause
